aec0bc061e3c9247e311abc05efc67068ac9d20a
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-22 17:01:09 +0530

    added landing login page UI

--- src/components/js/Login.js
+++ src/components/js/Login.js
@@ -5,7 +5,8 @@ import '../css/Login.css';
 function Login() {
     return (
         <div className="login">
-            <h1>Login</h1>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <a>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
