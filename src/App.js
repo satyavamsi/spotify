1a889b197a63be8fcb3393c9a1d674ab6513c5ef
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-22 17:40:31 +0530

    added redirect after authorisatin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Login from './components/js/Login';
+import { getTokenFroUrl } from './helpers/spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFroUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, [])
+
   return (
     <div className="app">
-      <Login />
+      {token ? (
+        <h1>I am logged in</h1>
+      ) : (
+          <Login />
+        )}
     </div>
   );
 }
--- src/helpers/spotify.js
+++ src/helpers/spotify.js
@@ -1,9 +1,13 @@
+// authorisation end point
 export const authEndPoint = 'https://accounts.spotify.com/authorize';
 
+// redirect uri after authorisation
 const redirectUri = "http://localhost:3000/";
 
+// developer client_id
 const clientId = "YOUR_SPOTIFY_DEVELOPER_CLIENT_ID";
 
+// user permissions for the token
 const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
@@ -12,5 +16,18 @@ const scopes = [
     "user-modify-playback-state"
 ];
 
+// get token from url after authorisation and redirect
+export const getTokenFroUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {})
+}
+
+// final login url with client_id,, redirect_uri and scopes
 export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20")}&response_type=token&show_dialog=true`;
